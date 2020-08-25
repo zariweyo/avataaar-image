ac466daa98a405e4b40362732b52b17af61dab63
tomwyr <tomwyr@hotmail.com>
2020-08-25 18:33:28 +0200

    Make base url variable

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -4,14 +4,14 @@ import 'package:avataaar_image/src/avataaar.dart';
 import 'package:http/http.dart' as http;
 
 class AvataaarsApi {
-  static const _BASE_URL = 'https://avataaars.io';
+  static String baseUrl = 'https://avataaars.io/';
 
   String getUrl(Avataaar avatar, double width) {
     final params = avatar.pieceEntries.map((it) {
       final key = it.key[0].toLowerCase() + it.key.substring(1);
       return '$key=${it.value}';
     }).join('&');
-    return '$_BASE_URL/png/$width?$params';
+    return '$baseUrl/png/$width?$params';
   }
 
   Future<Uint8List> getImage(Avataaar avatar, double width) =>
